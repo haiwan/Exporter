c6a2a4b0640c3241ec14880cd950dd49be939845
haijian <alwin.wang.oocl@gmail.com>
2019-05-10 23:08:45 +0300

    make xlsx default file extension for v8

--- exporter-addon/src/main/java/org/vaadin/haijian/Exporter.java
+++ exporter-addon/src/main/java/org/vaadin/haijian/Exporter.java
@@ -9,15 +9,11 @@ public class Exporter {
     private Exporter(){}
 
     public static <T> InputStream exportAsExcel(Grid<T> grid){
-        return new ExcelFileBuilder<>(grid).build();
+        return new XlsxFileBuilder<>(grid).build();
     }
 
     public static <T> InputStream exportAsXls(Grid<T> grid){
         return new XlsFileBuilder<>(grid).build();
-    }
-
-    public static <T> InputStream exportAsXlsx(Grid<T> grid){
-        return new XlsxFileBuilder<>(grid).build();
     }
 
     public static <T> InputStream exportAsCSV(Grid<T> grid){
