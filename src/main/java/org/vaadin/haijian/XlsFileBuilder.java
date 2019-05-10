42e7e083848bec4edac0bae3766f8e08887dd7d4
haijian <alwin.wang.oocl@gmail.com>
2019-05-10 22:47:49 +0300

    update readme file and some optimization

--- src/main/java/org/vaadin/haijian/XlsFileBuilder.java
+++ src/main/java/org/vaadin/haijian/XlsFileBuilder.java
@@ -3,7 +3,7 @@ package org.vaadin.haijian;
 import com.vaadin.flow.component.grid.Grid;
 
 public class XlsFileBuilder<T> extends ExcelFileBuilder<T>{
-    public XlsFileBuilder(Grid<T> grid) {
+    XlsFileBuilder(Grid<T> grid) {
         super(grid);
     }
 }
--- src/main/java/org/vaadin/haijian/XlsxFileBuilder.java
+++ src/main/java/org/vaadin/haijian/XlsxFileBuilder.java
@@ -7,7 +7,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class XlsxFileBuilder<T> extends ExcelFileBuilder<T>{
     private static final String EXCEL_FILE_EXTENSION = ".xlsx";
 
-    public XlsxFileBuilder(Grid<T> grid) {
+    XlsxFileBuilder(Grid<T> grid) {
         super(grid);
     }
 
