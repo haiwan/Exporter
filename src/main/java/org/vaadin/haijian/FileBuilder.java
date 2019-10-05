d40305ead9fd1882d0c172280157413ec08c6ba8
Krunoslav Magazin <krunoslav.magazin@gmail.com>
2019-10-05 15:48:52 +0200

    Update and fix demo.

--- src/main/java/org/vaadin/haijian/FileBuilder.java
+++ src/main/java/org/vaadin/haijian/FileBuilder.java
@@ -1,13 +1,5 @@
 package org.vaadin.haijian;
 
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.data.binder.BeanPropertySet;
-import com.vaadin.flow.data.binder.PropertyDefinition;
-import com.vaadin.flow.data.binder.PropertySet;
-import com.vaadin.flow.data.provider.DataCommunicator;
-import com.vaadin.flow.data.provider.Query;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -20,19 +12,28 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.data.binder.BeanPropertySet;
+import com.vaadin.flow.data.binder.PropertyDefinition;
+import com.vaadin.flow.data.binder.PropertySet;
+import com.vaadin.flow.data.provider.DataCommunicator;
+import com.vaadin.flow.data.provider.Query;
+
 public abstract class FileBuilder<T> {
     private static final String TMP_FILE_NAME = "tmp";
 
     File file;
     private Grid<T> grid;
-    private Map<Grid.Column<T>, String> columnHeaders;
+    private Map<Grid.Column<T>, String> customColumnHeaders;
     private Collection<Grid.Column> columns;
     private PropertySet<T> propertySet;
 
     @SuppressWarnings("unchecked")
     FileBuilder(Grid<T> grid, Map<Grid.Column<T>, String> columnHeaders) {
         this.grid = grid;
-        this.columnHeaders = columnHeaders;
+        this.customColumnHeaders = columnHeaders;
         columns = grid.getColumns().stream().filter(this::isExportable).collect(Collectors.toList());
         try {
             Field field = Grid.class.getDeclaredField("propertySet");
@@ -40,6 +41,9 @@ public abstract class FileBuilder<T> {
             Object propertySetRaw = field.get(grid);
             if (propertySetRaw != null) {
                 propertySet = (PropertySet<T>) propertySetRaw;
+            }
+            if (propertySet == null && columnHeaders == null) {
+                throw new ExporterException("Create Grid with bean type or create Grid with adding columns and columns headers map.");
             }
         } catch (Exception e) {
             throw new ExporterException("couldn't read propertyset information from grid", e);
@@ -83,29 +87,32 @@ public abstract class FileBuilder<T> {
     }
 
     private void buildHeaderRow() {
-      onNewRow();
-      if (columnHeaders == null) {
-        columns.forEach(column -> {
-            Optional<PropertyDefinition<T, ?>> propertyDefinition = propertySet.getProperty(column.getKey());
-            if (propertyDefinition.isPresent()) {
-                onNewCell();
-                buildColumnHeaderCell(propertyDefinition.get().getCaption());
-            } else {
-                LoggerFactory.getLogger(this.getClass()).warn(String.format("Column key %s is a property which cannot be found", column.getKey()));
-            }
-        });
-      } else {
-        columns.forEach(column -> { 
-          String columnHeader = columnHeaders.get(column);
-          if (columnHeader != null) {
-            onNewCell();
-            buildColumnHeaderCell(columnHeader);
-          } else {
-              LoggerFactory.getLogger(this.getClass()).warn(String.format("Column with key %s have not column header value defined in map", column.getKey()));
-          }
-        });
+        onNewRow();
+        if (customColumnHeaders == null) {
+            columns.forEach(column -> {
+                // when grid created with bean type
+                if (propertySet != null) {
+                    Optional<PropertyDefinition<T, ?>> propertyDefinition = propertySet.getProperty(column.getKey());
+                    if (propertyDefinition.isPresent()) {
+                        onNewCell();
+                        buildColumnHeaderCell(propertyDefinition.get().getCaption());
+                    } else {
+                        LoggerFactory.getLogger(this.getClass()).warn(String.format("Column key %s is a property which cannot be found", column.getKey()));
+                    }
+                }
+            });
+        } else {
+            columns.forEach(column -> {
+                String columnHeader = customColumnHeaders.get(column);
+                if (columnHeader != null) {
+                    onNewCell();
+                    buildColumnHeaderCell(columnHeader);
+                } else {
+                    LoggerFactory.getLogger(this.getClass()).warn(String.format("Column with key %s have not column header value defined in map", column.getKey()));
+                }
+            });
+        }
     }
-  }
 
 
     void buildColumnHeaderCell(String header) {
@@ -177,14 +184,12 @@ public abstract class FileBuilder<T> {
         return columns.size();
     }
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     private Stream<T> getDataStream(Query newQuery) {
         Stream<T> stream = grid.getDataProvider().fetch(newQuery);
         if (stream.isParallel()) {
-            LoggerFactory.getLogger(DataCommunicator.class)
-                    .debug("Data provider {} has returned "
-                                    + "parallel stream on 'fetch' call",
-                            grid.getDataProvider().getClass());
+            LoggerFactory.getLogger(DataCommunicator.class).debug("Data provider {} has returned " + "parallel stream on 'fetch' call",
+                    grid.getDataProvider().getClass());
             stream = stream.collect(Collectors.toList()).stream();
             assert !stream.isParallel();
         }
